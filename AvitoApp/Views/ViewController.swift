8eeec1ac4323f6212e28629cb978a9ee538cdfb4
Pavel <paveltihtey@gmail.com>
2022-07-05 00:58:22 +0300

    tableView added

--- AvitoApp/Views/ViewController.swift
+++ AvitoApp/Views/ViewController.swift
@@ -1,20 +1,38 @@
 import UIKit
 
-struct EmployeesJson: Decodable {
-    var employees: Participants?
+struct Company: Decodable {
+    let name: String?
+    var employees: [Participants]?
+}
+
+struct CompanyJson: Decodable {
+    let company: Company?
 }
 
 struct Participants: Decodable {
     var name: String?
     var phone_number: String?
-    var skills: Array <Any>?
+    var skills: [String]?
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case skills
+        case phone_number = "phone_number"
+    }
 }
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
     
+    let idCell = "idcell"
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
         
         let urlString = "https://run.mocky.io/v3/1d1cb4ec-73db-4762-8c4b-0b8aa3cecd4c"
         guard let url = URL (string: urlString) else {return}
@@ -24,12 +42,36 @@ class ViewController: UIViewController {
             
             
             do {
-                let employeesJson = try JSONDecoder().decode(EmployeesJson.self, from: data)
-                print (employeesJson)
+                let Company = try JSONDecoder().decode(Company.self, from: data)
+                print (Company)
                 
             } catch let error {
                 print (error)
             }
         }.resume()
+    }
+}
+
+
+extension ViewController:  UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: idCell)
+        if cell == nil {
+            cell = UITableViewCell (style: .subtitle, reuseIdentifier: idCell)
+            
+        }
+        
+        cell!.textLabel?.text = "Main text"
+        cell!.detailTextLabel?.text = "Tap to show more"
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
 }
